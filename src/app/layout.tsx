76ab277217b52aac91735e687bfc80f34169f6de
Grim Reaper <olusijackson@gmail.com>
2024-10-06 14:26:13 +0100

    font import fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,16 @@ import localFont from "next/font/local";
 import "./globals.css";
 // import 'react-hot-toast/dist/react-hot-toast.css';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "Summarizer App",
